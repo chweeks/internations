5051518500d35fe1726fd76a5633c5bbbaae01a1
chweeks <chrisharryweeks@gmail.com>
2016-01-27 21:37:49 +0000

    added addUser and addGroup functions

--- src/script.js
+++ src/script.js
@@ -1,13 +1,15 @@
-var addUserBtn = document.getElementById("addUser");
-var addGroupBtn = document.getElementById("addGroup");
-
-var addUser = () => {
-
+var addUser = (userName) => {
+  var list = document.createElement("LI");
+  var user = document.createTextNode(userName);
+  list.appendChild(user);
+  document.getElementById('userList').appendChild(list);
+  document.getElementById("userName").value = ""
 };
 
-var addGroup = () => {
-
+var addGroup = (groupName) => {
+  var list = document.createElement("LI");
+  var group = document.createTextNode(groupName);
+  list.appendChild(group);
+  document.getElementById('groupList').appendChild(list);
+  document.getElementById("groupName").value = ""
 };
-
-addUserBtn.addEventListener("click", "addUser");
-addGroupBtn.addEventListener("click", "addGoup")
